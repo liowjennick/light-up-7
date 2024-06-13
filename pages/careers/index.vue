1cd1bf581fef0fbb4469a4efb6502262be472c3a
alfie.tinkerve <ahmadizzman107@gmail.com>
2024-06-13 22:28:50 +0800

    view markup

---
+++ pages/careers/index.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="careers">
+    <!-- Banner -->
+    <div>
+      <!-- Animation here -->
+    </div>
+    <!-- Staff pics -->
+    <div>
+      <div>
+        <div>
+          <p>Join us in lighting up the world!</p>
+          <p>
+            All great things start with a spark—and at Light Up 7, we believe in
+            fanning the embers of creativity and success in each team member. We
+            are a proudly all-in and in-house team that provides support and
+            avenues for you to bring your aspirations and visions to life. When
+            you’re ready to light up your career, Light Up 7 is ready for you.
+          </p>
+        </div>
+        <div>
+          <img src="" width="707" />
+        </div>
+      </div>
+      <div>
+        <button>be part of LU7!</button>
+      </div>
+    </div>
+    <!-- Company values -->
+    <div>
+      <p>How we continue to shine</p>
+      <div>
+        <div>
+          <div>
+            <img src="" width="106" />
+          </div>
+          <div>
+            <p>Celebrating Creativity</p>
+          </div>
+          <div>
+            <p>
+              We believe the greatest ideas comes from the experience of being
+              human. Whatever shape or form, your creativity will always have a
+              seat at our table.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="" width="106" />
+          </div>
+          <div>
+            <p>Doing Work That Matters</p>
+          </div>
+          <div>
+            <p>
+              Joining Light Up 7 means being part of projects that make a
+              difference. Lead or have a hand in creating lasting impact with
+              brands and organisations across the APAC region.
+            </p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="" width="106" />
+          </div>
+          <div>
+            <p>Always Growing, Never Stopping</p>
+          </div>
+          <div>
+            <p>
+              We understand wanting to leave your mark in the industry and we
+              will support you until you make it! No matter the goals, you will
+              have the resources to thrive.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Company pics -->
+    <div>
+      <p>Our team at work (and play!)</p>
+      <div>
+        <div>
+          <img src="" width="477" />
+          <!-- overlay text -->
+          <p>
+            “I love how diverse the team is. You learn so much from people of
+            various backgrounds and plus point: more festive celebrations!’ F.I,
+            Head of Motion Design
+          </p>
+        </div>
+        <div>
+          <img src="" width="477" />
+          <!-- overlay text -->
+          <p>
+            “Super thankful for all the opportunities where the team trusted me
+            to take lead on projects. Truly a transformative experience at Light
+            Up 7.” I.S, Project Manager
+          </p>
+        </div>
+        <div>
+          <img src="" width="477" />
+          <!-- overlay text -->
+          <p>
+            “The work’s challenging but so is the downtime! Nothing beats a long
+            day of projects like one (or 5) rounds of pool.” M.S, Video
+            Production Specialist
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- Vacancies -->
+    <div>
+      <p>ready to explore a new path?</p>
+      <p>check out your dream role here.</p>
+      <!-- filter tabs -->
+      <div>
+        <div>
+          <div><button>all</button></div>
+          <div><button>finance/accounting</button></div>
+          <div><button>events</button></div>
+          <div><button>internship</button></div>
+          <div><button>client services</button></div>
+          <div><button>content</button></div>
+          <div><button>creative</button></div>
+          <div><button>video</button></div>
+          <div><button>web</button></div>
+        </div>
+      </div>
+      <!-- vacancy grid -->
+      <div>
+        <div>
+          <div><p>Job Title</p></div>
+          <div><button>apply now!</button></div>
+        </div>
+        <div>
+          <div><p>Job Title</p></div>
+          <div><button>apply now!</button></div>
+        </div>
+        <div>
+          <div><p>Job Title</p></div>
+          <div><button>apply now!</button></div>
+        </div>
+        <div>
+          <div><p>Job Title</p></div>
+          <div><button>apply now!</button></div>
+        </div>
+        <div>
+          <div><p>Job Title</p></div>
+          <div><button>apply now!</button></div>
+        </div>
+        <div>
+          <div><p>Job Title</p></div>
+          <div><button>apply now!</button></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style lang="sass">
+@import "../../assets/sass/responsive.sass"
+@import "../../assets/sass/fonts.sass"
+@import "../../assets/sass/animations.sass"
+@import "../../assets/sass/layout.sass"
+</style>
