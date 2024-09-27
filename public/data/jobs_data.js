a05cae676225bfaa7e2ecb1004c8f63724ffc7db
Amiruddin <amiruan12@gmail.com>
2024-09-27 11:27:59 +0800

    New Job added

--- public/data/jobs_data.js
+++ public/data/jobs_data.js
@@ -1,17 +1,19 @@
 import EventManager from "../data/jobs/event-manager.json"
-import FinanceAccountingExecutive from "../data/jobs/finance-accounting-executive.json"
 import Internship from "../data/jobs/internship.json"
 import JuniorProjectManager from "../data/jobs/junior-project-manager.json"
 import SeniorDigitalDesigner from "../data/jobs/senior-digital-designer.json"
 import JuniorDigitalDesigner from "../data/jobs/junior-digital-designer.json"
+import ArtDirector from "../data/jobs/art-director.json"
+import SeniorVideographerVideoEditor from "../data/jobs/senior-videographer-video-editor.json"
 
 const jobsData = [
-    EventManager,
-    FinanceAccountingExecutive,
     Internship,
+    EventManager,
     JuniorProjectManager,
+    ArtDirector,
     SeniorDigitalDesigner,
-    JuniorDigitalDesigner
+    JuniorDigitalDesigner,
+    SeniorVideographerVideoEditor
 ]
 
 export default jobsData
