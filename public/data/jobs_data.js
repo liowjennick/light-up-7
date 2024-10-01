33e81f6856d5a4da81d90b91a4ccec936b768631
Amiruddin <amiruan12@gmail.com>
2024-10-01 16:10:36 +0800

    Graphic designer job title updated

--- public/data/jobs_data.js
+++ public/data/jobs_data.js
@@ -2,16 +2,16 @@ import EventManager from "../data/jobs/event-manager.json"
 import FinanceAccountingExecutive from "../data/jobs/finance-accounting-executive.json"
 import Internship from "../data/jobs/internship.json"
 import JuniorProjectManager from "../data/jobs/junior-project-manager.json"
-import SeniorDigitalDesigner from "../data/jobs/senior-digital-designer.json"
-import JuniorDigitalDesigner from "../data/jobs/junior-digital-designer.json"
+import SeniorGraphicDesigner from "../data/jobs/senior-graphic-designer.json"
+import JuniorGraphicDesigner from "../data/jobs/junior-graphic-designer.json"
 
 const jobsData = [
     EventManager,
     FinanceAccountingExecutive,
     Internship,
     JuniorProjectManager,
-    SeniorDigitalDesigner,
-    JuniorDigitalDesigner
+    SeniorGraphicDesigner,
+    JuniorGraphicDesigner
 ]
 
 export default jobsData
