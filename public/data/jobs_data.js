d5a9bf91600de391860fd2de4f5d75c9f6e3d92f
Amiruddin <amiruan12@gmail.com>
2024-10-01 16:36:53 +0800

    Graphic designer job title x3

--- public/data/jobs_data.js
+++ public/data/jobs_data.js
@@ -1,8 +1,8 @@
 import EventManager from "../data/jobs/event-manager.json"
 import Internship from "../data/jobs/internship.json"
 import JuniorProjectManager from "../data/jobs/junior-project-manager.json"
-import SeniorDigitalDesigner from "../data/jobs/senior-digital-designer.json"
-import JuniorDigitalDesigner from "../data/jobs/junior-digital-designer.json"
+import SeniorGraphicDesigner from "../data/jobs/senior-graphic-designer.json"
+import JuniorGraphicDesigner from "../data/jobs/junior-graphic-designer.json"
 import ArtDirector from "../data/jobs/art-director.json"
 import SeniorVideographerVideoEditor from "../data/jobs/senior-videographer-video-editor.json"
 
@@ -11,8 +11,8 @@ const jobsData = [
     EventManager,
     JuniorProjectManager,
     ArtDirector,
-    SeniorDigitalDesigner,
-    JuniorDigitalDesigner,
+    SeniorGraphicDesigner,
+    JuniorGraphicDesigner,
     SeniorVideographerVideoEditor
 ]
 
