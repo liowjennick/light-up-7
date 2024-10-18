79bef2abb0d9d36133ca3f6057a97cc0fcbb066f
Amiruddin <amiruan12@gmail.com>
2024-10-18 11:11:45 +0800

    Balang Kuih Abah article added

--- public/data/news_data.js
+++ public/data/news_data.js
@@ -1,9 +1,11 @@
 import SME100Awards from "../data/news/sme-100-awards.json"
 import LU7PrestigiousAgencyAward2022 from "../data/news/lu7-prestigious-agency-award-2022.json"
+import MeetingFamiliesHalfwayRayaPnbAsnb from "../data/news/meeting-families-halfway-raya-pnb-asnb.json"
 
 const newsData = [
     SME100Awards,
-    LU7PrestigiousAgencyAward2022
+    LU7PrestigiousAgencyAward2022,
+    MeetingFamiliesHalfwayRayaPnbAsnb
 ]
 
 export default newsData
