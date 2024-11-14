dab13bb03673f67a75fe33795498a6dba5886070
Amiruddin <amiruan12@gmail.com>
2024-11-14 14:57:58 +0800

    add new News

--- public/data/news_data.js
+++ public/data/news_data.js
@@ -1,11 +1,16 @@
 import SME100Awards from "../data/news/sme-100-awards.json"
 import LU7PrestigiousAgencyAward2022 from "../data/news/lu7-prestigious-agency-award-2022.json"
 import MeetingFamiliesHalfwayRayaPnbAsnb from "../data/news/meeting-families-halfway-raya-pnb-asnb.json"
+import Lu7BullAward2024 from "../data/news/lu7-bull-award-2024.json" 
+import Lu7DragonAward2024 from "../data/news/lu7-dragon-award-2024.json"
 
 const newsData = [
+    Lu7DragonAward2024,
+    Lu7BullAward2024,
     MeetingFamiliesHalfwayRayaPnbAsnb,
     SME100Awards,
     LU7PrestigiousAgencyAward2022
+    
     
 ]
 
