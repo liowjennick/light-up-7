8664aa156c2461413d887ac58efa8c4dac3e4a40
Amiruddin <amiruan12@gmail.com>
2025-03-27 09:51:10 +0800

    Hiredly article added

--- public/data/news_data.js
+++ public/data/news_data.js
@@ -5,9 +5,11 @@ import Lu7BullAward2024 from "../data/news/lu7-bull-award-2024.json"
 import Lu7DragonAward2024 from "../data/news/lu7-dragon-award-2024.json"
 import Lu7GalaDinner from "../data/news/lu7-10th-year-anniversary-gala-dinner.json"
 import Lu7PhuketEscapade from "../data/news/lu7-phuket-escapade.json"
-import Lu7isNowInAustralia from "../data/news/lu7-is-now-in-australia.json"
+import Lu7isNowInAustralia from "../data/news/lu7-is-now-in-australia.json" 
+import LifeatLightUp7Unfiltered from "../data/news/life-at-lu7-unfiltered.json" 
 
 const newsData = [
+    LifeatLightUp7Unfiltered,
     Lu7isNowInAustralia ,
     Lu7DragonAward2024,
     Lu7PhuketEscapade,
